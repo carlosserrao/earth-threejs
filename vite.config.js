6aeaee6c6a4da0f7191f5c42cb830a20622ca55f
Carlos Fernandes <carlosjsf89@gmail.com>
2025-12-17 13:34:47 +0000

    Enable local server on Vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src', // Set the root directory to /src
-  publicDir: '../static/'
+  publicDir: '../static/',
+  server: {
+    host: true
+  }
 });
